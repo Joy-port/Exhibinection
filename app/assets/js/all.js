fac1dec8dcad525d905f6462118eec0c06205ac3
Joy Cheng <joy.cheng97324@gmail.com>
2021-08-13 16:44:02 +0800

    add index page+RWD

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,3 +1,16 @@
-$(function() {
-  console.log('Hello Bootstrap5');
+//swipers
+//https://swiperjs.com/demos
+
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  freeMode: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
 });
