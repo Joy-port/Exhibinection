5f247a9cf5760baabf6f4871bd6c886d67968791
Joy Cheng <joy.cheng97324@gmail.com>
2021-08-13 16:43:37 +0800

    Update 2021-08-13T08:43:36.538Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,6 +1,18 @@
 "use strict";
 
-$(function () {
-  console.log('Hello Bootstrap5');
+//swipers
+//https://swiperjs.com/demos
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  freeMode: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev"
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true
+  }
 });
 //# sourceMappingURL=all.js.map
