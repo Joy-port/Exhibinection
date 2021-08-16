b36fcd8d03df1a1cf4aae22b8e9244d81fe1fcf7
Joy Cheng <joy.cheng97324@gmail.com>
2021-08-16 12:26:43 +0800

    add payment page

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -13,3 +13,16 @@ var swiper = new Swiper(".mySwiper", {
     clickable: true,
   },
 });
+
+//ticket submit//notyet done
+const ticketSubmit = document.querySelector('.ticket-purchase-done');
+
+ticketSubmit = addEventListener("click",function(e){
+  if(e.target.getAttribute("class")!=="ticket-purchase-done"){
+    console.log('這邊不用顯示alert');
+    return;
+  }else{
+    console.log('這邊需要顯示alert');
+  alert("訂購已完成，請至信箱查看相關訂購資訊");
+}
+})
