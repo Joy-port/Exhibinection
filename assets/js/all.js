b9b953c362d332819d49299f6a91b290cbef40f3
Joy Cheng <joy.cheng97324@gmail.com>
2021-08-16 12:26:21 +0800

    Update 2021-08-16T04:25:46.861Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,5 +1,7 @@
 "use strict";
 
+function _readOnlyError(name) { throw new TypeError("\"" + name + "\" is read-only"); }
+
 //swipers
 //https://swiperjs.com/demos
 var swiper = new Swiper(".mySwiper", {
@@ -14,5 +16,16 @@ var swiper = new Swiper(".mySwiper", {
     el: ".swiper-pagination",
     clickable: true
   }
-});
+}); //ticket submit//notyet done
+
+var ticketSubmit = document.querySelector('.ticket-purchase-done');
+addEventListener("click", function (e) {
+  if (e.target.getAttribute("class") !== "ticket-purchase-done") {
+    console.log('這邊不用顯示alert');
+    return;
+  } else {
+    console.log('這邊需要顯示alert');
+    alert("訂購已完成，請至信箱查看相關訂購資訊");
+  }
+}), _readOnlyError("ticketSubmit");
 //# sourceMappingURL=all.js.map
