9d29ea2ca38862dc877b6cde6426d5ad76b38dbb
Joy Cheng <joy.cheng97324@gmail.com>
2021-08-20 17:20:17 +0800

    [edit]scss helpers folder

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -32,3 +32,25 @@ var swiper = new Swiper(".swiper-js", {
     },
   },
 });
+
+//bs5 validation 
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+(function () {
+  'use strict'
+
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  var forms = document.querySelectorAll('.needs-validation')
+
+  // Loop over them and prevent submission
+  Array.prototype.slice.call(forms)
+    .forEach(function (form) {
+      form.addEventListener('submit', function (event) {
+        if (!form.checkValidity()) {
+          event.preventDefault()
+          event.stopPropagation()
+        }
+
+        form.classList.add('was-validated')
+      }, false)
+    })
+})()
